70f2c4a20a7a8026b7a8b420caf53e4ce7d70f1d
Georgii Bondarenko <bgv900@mail.ru>
2023-03-09 19:50:02 +0300

    Task_8

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear ();
+Console.WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+
+int N;
+int n = 0;
+int ost= (n % 2);
+Console.Write("Введите число N: ");
+int.TryParse(Console.ReadLine ()!, out N);
+
+while (n <= N)
+{
+    if (ost == 0) 
+    {
+Console.Write($"{n}, ");
+    n = n + 2;
+    } 
+    else
+    {
+    n = n +1;
+    }
+}
